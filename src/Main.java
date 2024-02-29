5aa4f18d77b7b67b6141af60061ac1daaa82814c
Darius <103670474+Dari-OS@users.noreply.github.com>
2024-02-29 16:17:53 +0100

    fixed some things

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,11 @@ public class Main {
         CopyEventHandler copyEventHandler = new CopyEventHandler();
             if (args.length != 0) {
                 copyEventHandler.setAuthenticationKey(args[0]);
+            } else {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Please provide an api key for chatGPT:");
+                copyEventHandler.setAuthenticationKey(scanner.next());
             }
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please provide an api key for chatGPT:");
-        copyEventHandler.setAuthenticationKey(scanner.next());
         copyEventHandler.start();
 
 
